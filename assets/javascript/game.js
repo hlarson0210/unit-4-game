389342554b08c37900eaea37c1176e1872f21176
Hayden Larson <hlarson398@gmail.com>
2019-07-23 23:03:47 -0500

    modified the .js and added the functions with if statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,75 @@
 var randomResult;
 var wins = 0;
 var losses = 0;
-var images = ["../images/477295.jpg", "..images/images-1.jpg", "../images/images.jpg", "../images/yellow_gem.jpg"]
-
-randomResult = Math.floor(Math.random() * 101) + 19;
-console.log(randomResult);
-
-$(".game-score").html(randomResult);
+// var images = ["../images/477295.jpg", "../images/images-1.jpg", "../images/images.jpg", "../images/yellow_gem.jpg"];
 
 
-for(var i = 0; i < 4; i++){
-    var random = Math.floor((Math.random() * 11) + 1);
-    console.log(random);
+var resetAndStart = function () {
 
-    var crystal = $("<div>");
+    $(".crystals").empty();
+
+    randomResult = Math.floor(Math.random() * 101) + 19;
+    console.log(randomResult);
+
+    $(".game-score").html(randomResult);
+
+
+    for (var i = 0; i < 4; i++) {
+        var random = Math.floor((Math.random() * 11) + 1);
+        console.log(random);
+
+        var crystal = $("<div>");
         crystal.attr({
             "class": 'button',
             "data-random": random
         });
-        crystal.css({
-            "background-image":"url('" + images[i] + "')",
-            "background-size":"cover"
-        });
+        // This line of code was to create an array for the images to be put into the above created div for the .crystal
+        // crystal.css({
+        //     "background-image":"url('" + images[i] + "')",
+        //     "background-size":"cover"
+        // });
 
-    $(".buttons").append(crystal);
+        $(".buttons").append(crystal);
+    }
 }
 
-$(".button").on("click", function () {
-    
-    console.log(($(this).attr("data-random")));
+resetAndStart();
 
+$(document).on("click", '.crystal', function () {
+
+    var num = parseInt($(this).attr("data-random"));
+
+    previous += num;
+
+
+    $("#previous").html("Total score: " + previous);
+
+    console.log(previous);
+
+    if (previous > random_result) {
+
+        losses++;
+
+        $("#losses").html("Losses: " + losses);
+
+        previous = 0;
+
+        resetAndStart();
+
+    }
+    else if (previous === random_result) {
+
+        wins++;
+
+        $("#wins").html("Wins: " + wins);
+
+        previous = 0;
+
+        resetAndStart();
+
+    }
 })
 
-if (random === randomResult) {
-    wins++;
-
-}
 
 
 
